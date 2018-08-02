a05dbd7649e25f3ce6e194688459cf20bdbf78d9
mpp <ppmiao0628@outlook.com>
2018-08-02 21:39:02 +0800

    first cm

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!--<router-view/>-->
+    <!--<toast></toast>-->
+    <input type="button" value="显示弹窗" @click="showToast"/>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    showToast () {
+      this.$toast('我就是弹窗呀')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/toast/Toast.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrap">
+    在下是弹窗
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toast',
+  data: function () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .wrap {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    background: rgba(0, 0, 0, .35);
+    padding: 10px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    -o-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    color: #fff;
+  }
+</style>
